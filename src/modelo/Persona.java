267c122053326198b3ab5c87a93101d60a94cf56
Bustamantes <edwin.bustamante0880@gmail.com>
2025-07-16 09:45:59 -0500

    Update Persona.java

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,1 +1,44 @@
+package modelo;
+
+public class Persona {
+    protected String identificacion;
+    protected String nombre;
+    protected String apellido;
+
+    public Persona(String identificacion, String nombre, String apellido) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String identificar() {
+        return nombre + " " + apellido + " (" + identificacion + ")";
+    }
+
+    // Getters
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    // Setters
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
 
