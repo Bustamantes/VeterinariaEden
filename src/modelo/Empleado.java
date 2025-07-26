d6995a9afa1c3c7de6355772bdd2e2773b6f7240
Bustamantes <edwin.bustamante0880@gmail.com>
2025-07-26 05:36:19 -0500

    Update Empleado.java

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -1,1 +1,30 @@
+package modelo;
 
+public class Empleado extends Persona {
+
+    private TipoEmpleado tipo;
+
+    public Empleado(String identificacion, String nombre, String apellido, TipoEmpleado tipo) {
+        super(identificacion, nombre, apellido);
+        this.tipo = tipo;
+    }
+
+    public TipoEmpleado getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEmpleado tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String identificar() {
+        return super.identificar() + " - Tipo: " + tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + tipo + "] " + getNombre() + " " + getApellido() +
+               " (ID: " + getIdentificacion() + ")";
+    }
+}
