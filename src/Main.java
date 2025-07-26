c7ec49005a8fce7444ae03b8470562b58134efcf
Bustamantes <edwin.bustamante0880@gmail.com>
2025-07-26 10:51:13 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
-// Todo junto
+// src/Main.java
+import ui.MenuPrincipal;
 
-// Hello
+public class Main {
+    public static void main(String[] args) {
+        // Delegamos al menú principal, que ya carga datos de ejemplo y muestra la UI
+        MenuPrincipal.main(args);
+    }
+}
